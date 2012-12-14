8850c2d2a8235c54295caa1178802b233327734a
makskay <maxkreminski@gmail.com>
2012-12-13 19:58:50 -0500

    Fix /issues output pagination

--- src/me/makskay/tidy/commands/IssuesCommand.java
+++ src/me/makskay/tidy/commands/IssuesCommand.java
@@ -13,6 +13,7 @@ import org.bukkit.command.CommandSender;
 
 public class IssuesCommand implements CommandExecutor {
 	private IssueManager issueManager;
+	private final int ISSUES_PER_PAGE = 8;
 	
 	public IssuesCommand(TidyPlugin plugin) {
 		issueManager = plugin.getIssueManager();
@@ -20,36 +21,49 @@ public class IssuesCommand implements CommandExecutor {
 	
 	public boolean onCommand (CommandSender sender, Command command, String commandLabel, String[] args) {
 		int pageNumber = 0;
-		int issuesPerPage = 8; // TODO this should be configurable
 		boolean searchAllIssues = false;
 		
+		/*
+		 * Parse the input (including page number and flags if applicable.)
+		 */
+		
 		if (args.length == 0) {
-			// do nothing
+			// Do nothing
 		}
 		
 		else if (args.length == 1) {
 			try {
-				pageNumber = Integer.parseInt(args[0]);
-			} catch (NumberFormatException ex) {
-				if (!args[0].equals("-all")) {
-					sender.sendMessage(TidyPlugin.ERROR_COLOR + "\"" +  args[0] + "\" isn't a valid page number");
-					return true;
+				pageNumber = Integer.parseInt(args[0]) - 1;
+			} 
+			
+			catch (NumberFormatException ex) {
+				if (args[0].equalsIgnoreCase("-all")) {
+					searchAllIssues = true;
 				}
 				
-				searchAllIssues = true;
+				else {
+					sender.sendMessage(TidyPlugin.ERROR_COLOR + "\"" +  args[0] + "\" isn't a valid page number!");
+					return true;
+				}
 			}
 		}
 		
 		else if (args.length == 2) {
 			try {
-				pageNumber = Integer.parseInt(args[0]);
-			} catch (NumberFormatException ex) {
-				sender.sendMessage(TidyPlugin.ERROR_COLOR + "\"" +  args[0] + "\" isn't a valid page number");
+				pageNumber = Integer.parseInt(args[0]) - 1;
+			} 
+			
+			catch (NumberFormatException ex) {
+				sender.sendMessage(TidyPlugin.ERROR_COLOR + "\"" +  args[0] + "\" isn't a valid page number!");
 				return true;
 			}
 			
-			if (!args[1].equals("-all")) {
-				sender.sendMessage(TidyPlugin.ERROR_COLOR + "\"" +  args[1] + "\" isn't a valid flag");
+			if (args[1].equalsIgnoreCase("-all")) {
+				searchAllIssues = true;
+			}
+			
+			else {
+				sender.sendMessage(TidyPlugin.ERROR_COLOR + "\"" +  args[1] + "\" isn't a valid flag!");
 				return true;
 			}
 		}
@@ -58,70 +72,82 @@ public class IssuesCommand implements CommandExecutor {
 			return false;
 		}
 		
-		int startIndex = pageNumber * issuesPerPage;
-		List<IssueReport> issues = issueManager.getCachedIssues();
+		/*
+		 * Generate a list of issues matching the search query.
+		 */
 		
-		if (searchAllIssues) { // generate a list of ALL issues (resolved included) at the specified page
+		int startIndex = pageNumber * ISSUES_PER_PAGE;
+		pageNumber++; // Label "page 0" (array indices 0-7) as page 1 and so forth
+		
+		List<IssueReport> allIssues = issueManager.getCachedIssues();
+		List<IssueReport> searchResults = new ArrayList<IssueReport>();
+		
+		if (searchAllIssues) {
 			if (sender.hasPermission("tidy.staff")) {
-				try {
-					issues = issues.subList(startIndex, startIndex + issuesPerPage - 1);
-				}
-				
-				catch (IndexOutOfBoundsException ex) {
-					try {
-						issues = issues.subList(startIndex, issues.size());
-					}
-					
-					catch (IndexOutOfBoundsException ex2) {
-						issues = issues.subList(0, issues.size());
-					}
-				}
+				searchResults.addAll(allIssues);
 			}
 			
 			else {
-				issues = new ArrayList<IssueReport>();
-				for (IssueReport issue : issueManager.getCachedIssues()) {
-					if (issues.size() > issuesPerPage) {
-						break;
-					}
-					
+				for (IssueReport issue : allIssues) {
 					if (issue.canBeEditedBy(sender)) {
-						issues.add(issue);
+						searchResults.add(issue);
 					}
 				}
 			}
 		}
 		
-		else { // generate a list of OPEN issues (iterate over the cache and check for !resolved) at the specified page
-			issues = new ArrayList<IssueReport>();
-			List<IssueReport> cachedIssues = issueManager.getCachedIssues();
-			int cacheSize = cachedIssues.size();
-			
-			for (IssueReport issue : cachedIssues) {
-				int size = issues.size();
-				if (size > issuesPerPage || size == cacheSize) {
-					break;
-				}
-				
+		else {
+			for (IssueReport issue : allIssues) {
 				if (issue.isOpen() && issue.canBeEditedBy(sender)) {
-					issues.add(issue);
+					searchResults.add(issue);
 				}
 			}
 		}
 		
-		if (issues.size() == 0) {
-			sender.sendMessage(TidyPlugin.ERROR_COLOR + "No issues matching the search criteria could be found");
+		/*
+		 * Appropriately edit down the list of matching issues.
+		 */
+		
+		int countIssuesRetrieved = searchResults.size();
+		int countPagesRetrieved = (countIssuesRetrieved % ISSUES_PER_PAGE == 0) ? 
+				(countIssuesRetrieved / ISSUES_PER_PAGE) : ((countIssuesRetrieved / ISSUES_PER_PAGE) + 1);
+		
+		if (countIssuesRetrieved == 0) {
+			sender.sendMessage(TidyPlugin.ERROR_COLOR + "No issues matching the search criteria could be found.");
+			
 			if (!searchAllIssues) {
 				sender.sendMessage(TidyPlugin.NEUTRAL_COLOR + 
-						"Consider broadening the search to include resolved issues: /issues -all");
+						"Consider broadening the search to include resolved issues: /issues [page number] -all");
 			}
+			
 			return true;
 		}
 		
-		sender.sendMessage(TidyPlugin.NEUTRAL_COLOR + "-- Issues --");
-		for (IssueReport issue : issues) {
+		try {
+			searchResults = searchResults.subList(startIndex, startIndex + ISSUES_PER_PAGE);
+		}
+		
+		catch (Exception ex1) {
+			try {
+				searchResults = searchResults.subList(startIndex, countIssuesRetrieved);
+			}
+			
+			catch (Exception ex2) {
+				searchResults = searchResults.subList(0, countIssuesRetrieved);
+			}
+		}
+		
+		/*
+		 * Print the edited-down list of search results.
+		 */
+		
+		sender.sendMessage(TidyPlugin.NEUTRAL_COLOR + "Issues (page " + TidyPlugin.HIGHLIGHT_COLOR + pageNumber + TidyPlugin.NEUTRAL_COLOR + 
+				"/" + TidyPlugin.HIGHLIGHT_COLOR + countPagesRetrieved + TidyPlugin.NEUTRAL_COLOR + ")");
+		
+		for (IssueReport issue : searchResults) {
 			sender.sendMessage(issue.shortSummary());
 		}
+		
 		return true;
 	}
 }
